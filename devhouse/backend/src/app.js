d8809aebc7534401353e0de7ffe0a21d7129da0f
Henrique Albuquerque <henriquealbuquerquef@gmail.com>
2021-08-05 19:43:35 -0300

    first commit

---
+++ devhouse/backend/src/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes';
+import path from 'path';
+
+class App {
+
+  constructor() {
+    this.server = express();
+
+    mongoose.connect('mongodb+srv://henrique-af:devhouse@devhouse.kgmmk.mongodb.net/DevHouse?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'uploads'))
+    );
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+}
+
+export default new App().server;
